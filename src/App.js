2710e8f3a86d5d2b23162eb551e5864bf16e4726
timcodes <hardtimo@isu.edu>
2020-09-18 17:41:08 -0600

    users table working with test data and added header to users

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Container from "@material-ui/core/Container";
+
+import logo from "./logo.svg";
+import "./App.css";
+
+import Users from "./Users";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Hello world
-        </a>
-      </header>
-    </div>
+    <Container component="main">
+      <Users />
+    </Container>
   );
 }
 
--- src/UsersTable.js
+++ src/UsersTable.js
@@ -1,7 +1,51 @@
 import * as React from "react";
+import { DataGrid } from "@material-ui/data-grid";
+import Avatar from "@material-ui/core/Avatar";
 
-const UsersTable = () => {
-  return "Users Table ";
-};
+const columns = [
+  {
+    field: "name",
+    headerName: "Team Member",
+    width: 180,
+    renderCell: (params) => {
+      return (
+        <>
+          <Avatar
+            alt="Avatar Image"
+            src={params.getValue("cached_avatar")}
+            component="span"
+            style={{ marginRight: "10px" }}
+          />
+          {"    "}
+          {params.value}{" "}
+        </>
+      );
+    },
+  },
+  { field: "email", headerName: "Email", width: 200 },
+  {
+    field: "stats",
+    headerName: "Invites",
+    width: 180,
+    valueGetter: (params) => `${params.value["invited_users_count"] || ""}`,
+  },
+  {
+    field: "stats",
+    headerName: "campaigns",
+    width: 180,
+    valueGetter: (params) =>
+      `${params.value["published_campaigns_count"] || ""}`,
+  },
+  { field: "created_at", headerName: "Date Created", width: 180 },
+];
+
+const UsersTable = ({ users }) => (
+  <DataGrid
+    rows={users}
+    columns={columns}
+    pageSize={5}
+    disableMultipleSelection
+  />
+);
 
 export default UsersTable;
