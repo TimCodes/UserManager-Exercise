40c62fb9f67c6755435bae00365c7e1f355f7bcd
TimCodes <hardtimo@isu.edu>
2020-09-21 13:05:18 -0600

    Update DataUtils.js
    Added comments to DataUtils

--- src/common/DataUtils.js
+++ src/common/DataUtils.js
@@ -1,12 +1,17 @@
+// Builds summary stats and sorts user data
 const FormatUserData = (users) => {
   let userData = users.reduce(
     (acc, user) => {
       const { invited_users_count, published_campaigns_count } = user.stats;
+
+      // add camapign if not null, could use type coercion here 
+      // instead to convert null to 0
       acc.totalCampaigns += published_campaigns_count
         ? published_campaigns_count
         : 0;
       acc.totalUserInvites += invited_users_count ? invited_users_count : 0;
-
+      
+      // convert to date obj for easier sorting
       user["created_at"] = new Date(user["created_at"]);
       acc.users.push(user);
 
@@ -15,11 +20,13 @@ const FormatUserData = (users) => {
 
     { totalCampaigns: 0, totalUserInvites: 0, users: [] }
   );
-
+  
+  // sort users desc by created_at date 
   userData.users.sort((a, b) => {
     return b["created_at"] - a["created_at"];
   });
 
+  // convert to datestring so react can render
   userData.lastCreatedAt = userData.users[0].created_at.toDateString();
   userData.totalteamMembers = userData.users.length;
 
